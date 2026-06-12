72b33d8ea6e7e65cc8d8d41ce6d36f240775b272
Yarden Zilberman <103817918+idont12@users.noreply.github.com>
2026-06-12 11:17:25 +0300

    Added base path and built production files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
+  // ADD THIS LINE (Replace 'your-repository-name' with your actual GitHub repo name)
+  base: '/Cardboard_Digital_Studio/', 
+  
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
